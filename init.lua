563c31c52ebe2487cbe27b3e7a30a608b395788a
maidenlessGxd <cokakwalker403@gmail.com>
2024-07-07 10:22:38 +0000

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,2 +1,27 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local plugins = {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  lazy = true,
+}
+
+require("catppuccin").setup({
+  flavour = "mocha",
+  transparent_background = true,
+  integrations = {
+    cmp = true,
+    mason = true,
+    noice = true,
+    notify = true,
+    harpoon = true,
+    neotree = true,
+    telescope = true,
+    treesitter = true,
+    which_key = true,
+  },
+})
+
+vim.cmd.colorscheme("catppuccin")
